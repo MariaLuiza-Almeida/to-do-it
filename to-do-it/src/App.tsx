c84ecb126bf8d109247128264dc7923418db5496
Maria Luiza <marialuiza.almeidafernandes@gmail.com>
2023-07-04 20:03:52 -0300

    :tada: vite configuration

---
+++ to-do-it/src/App.tsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <Fragment>
+      <p>Iniciando o projeto</p>
+    </Fragment>
+  );
+}
+
+export default App;
